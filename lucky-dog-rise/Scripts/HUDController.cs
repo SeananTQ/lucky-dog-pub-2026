1a0275bbd8c70c96c971c2c2f7363748a91aaf50
carlostq <carlostq@aliyun.com>
2026-06-19 22:47:09 +0800

    feat:清理了过时的HUD

--- lucky-dog-rise/Scripts/HUDController.cs
+++ lucky-dog-rise/Scripts/HUDController.cs
@@ -4,20 +4,21 @@ namespace LuckyDogRise;
 
 public partial class HUDController : CanvasLayer
 {
-    private Label _messageLabel = null!;
+    private Control _messagePanel = null!;
     private CanvasLayer _overlay = null!;
     private Label _centerLabel = null!;
 
     public override void _Ready()
     {
-        _messageLabel = GetNode<Label>("MessagePanel/MessageLabel");
+        _messagePanel = GetNode<Control>("MessagePanel");
+        _messagePanel.Visible = false;
         _overlay = GetParent().GetNode<CanvasLayer>("Overlay");
         _centerLabel = _overlay.GetNode<Label>("OverlayPanel/OverlayVBox/CenterLabel");
     }
 
     public void SetMessage(string msg)
     {
-        _messageLabel.Text = msg;
+        _messagePanel.Visible = false;
     }
 
     public void ShowOverlay(string text)
